6f9c2f2cfb096374088512fb353867a8e9c669be
mpoeverlein <max.poeverlein@gmail.com>
2025-03-26 18:37:43 +0100

    put shaders into helper file

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -5,6 +5,7 @@
  
 #include "linmath.h"
 #include "rainbow.h"
+#include "shaders.h"
  
 #include <stdlib.h>
 #include <stddef.h>
@@ -43,27 +44,6 @@ typedef struct SampleDimensions
     float dy;
 } SampleDimensions;
 
-// TODO: read shaders from file
-static const char* vertex_shader_text =
-"#version 330\n"
-"uniform mat4 MVP;\n"
-"in vec3 vCol;\n"
-"in vec2 vPos;\n"
-"out vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
- 
-static const char* fragment_shader_text =
-"#version 330\n"
-"in vec3 color;\n"
-"out vec4 fragment;\n"
-"void main()\n"
-"{\n"
-"    fragment = vec4(color, 1.0);\n"
-"}\n";
  
 static void error_callback(int error, const char* description)
 {
---
+++ shaders.h
@@ -0,0 +1,20 @@
+static const char* vertex_shader_text =
+"#version 330\n"
+"uniform mat4 MVP;\n"
+"in vec3 vCol;\n"
+"in vec2 vPos;\n"
+"out vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+ 
+static const char* fragment_shader_text =
+"#version 330\n"
+"in vec3 color;\n"
+"out vec4 fragment;\n"
+"void main()\n"
+"{\n"
+"    fragment = vec4(color, 1.0);\n"
+"}\n";
